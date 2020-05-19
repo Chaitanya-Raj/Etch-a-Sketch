7e40be2ccacda51ef3cf9ba8af4ffdd289ada93b
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-19 11:46:03 +0530

    implement all functions

--- main.js
+++ main.js
@@ -1,94 +1,85 @@
-let grid = document.getElementById('grid-container');
-let grey = document.getElementById('grey');
-let color = document.getElementById('color');
-let erase = document.getElementById('erase');
-let reset = document.getElementById('reset');
+let grid = document.getElementById("grid-container");
+let grey = document.getElementById("grey");
+let color = document.getElementById("color");
+let erase = document.getElementById("erase");
+let reset = document.getElementById("reset");
 
 function random256() {
-    return Math.floor(Math.random() * 256);
-}
-
-function defaultGrid(size) {
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            let cell = document.createElement('div');
-            cell.classList.add("cell");
-            // cell.addEventListener('mouseenter', () => {
-            //     let greyScale = random256();
-            //     cell.style.backgroundColor = `rgb(${greyScale}, ${greyScale}, ${greyScale})`;
-            //     cell.style.transition = '0.3s';
-            // });
-            grid.appendChild(cell);
-        }
-    }
-}
-
-function paintGrey() {
-    let cells = [...document.querySelectorAll(".cells")];
-    cells.forEach(cell => {
-        cell.addEventListener('mouseenter', () => {
-            let greyScale = random256();
-            cell.style.backgroundColor = `rgb(${greyScale}, ${greyScale}, ${greyScale})`;
-            cell.style.transition = '0.3s';
-        });
-    });
-}
-
-function paintColor() {
-    let cells = [...document.querySelectorAll(".cells")];
-    cells.forEach(cell => {
-        cell.addEventListener('mouseenter', () => {
-            cell.style.backgroundColor = `rgb(${random256()}, ${random256()}, ${random256()})`;
-            cell.style.transition = '0.3s';
-        });
-    });
-}
-
-function eraseCells() {
-    let cells = [...document.querySelectorAll(".cells")];
-    cells.forEach(cell => {
-        cell.addEventListener('mouseenter', () => {
-            cell.style.backgroundColor = white;
-        });
-    });
-}
-
-function removeCells() {
-    let cells = [...document.querySelectorAll(".cells")];
-    cells.forEach(cell => {
-        cell.remove();
-    });
-}
-
-function resetAll() {
-    removeGrid();
-    let size;
-    do {
-        size = prompt('Select a grid size between 2 and 100', '');
-    } while (size < 2 || size > 100);
-    newGrid(size);
-    defaultGrid(size);
+  return Math.floor(Math.random() * 256);
 }
 
 function newGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      grid.appendChild(cell);
+    }
+  }
 }
 
-grey.addEventListener('click', () => {
-    paintGrey();
+function removeGrid() {
+  let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    grid.removeChild(cell);
+  });
+}
+
+function resetAll() {
+  removeGrid();
+  let size;
+  do {
+    size = prompt("Select a grid size between 2 and 100", "");
+  } while (size < 2 || size > 100);
+  newGrid(size);
+}
+
+reset.addEventListener("click", () => {
+  resetAll();
 });
 
-color.addEventListener('click', () => {
-    paintColor();
+function paintGrey() {
+  let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      let greyScale = random256();
+      cell.style.backgroundColor = `rgb(${greyScale}, ${greyScale}, ${greyScale})`;
+      cell.style.transition = "0.3s";
+    });
+  });
+}
+
+grey.addEventListener("click", () => {
+  paintGrey();
 });
 
-erase.addEventListener('click', () => {
-    eraseCells();
+function paintColor() {
+  let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor = `rgb(${random256()}, ${random256()}, ${random256()})`;
+      cell.style.transition = "0.3s";
+    });
+  });
+}
+
+color.addEventListener("click", () => {
+  paintColor();
 });
 
-reset.addEventListener('click', () => {
-    resetAll();
+function eraseCells() {
+  let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor = "white";
+    });
+  });
+}
+
+erase.addEventListener("click", () => {
+  eraseCells();
 });
 
-defaultGrid(16);
+window.addEventListener("load", () => resetAll());
